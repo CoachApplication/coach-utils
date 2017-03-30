804a5024e6ae9b2079dd512ae52074fda9a4c5b0
james.nesbitt@wunder.io <james.nesbitt@wunder.io>
2017-03-30 12:30:05 +0300

    some more documentation

--- readerclosedecorator.go
+++ readerclosedecorator.go
@@ -2,6 +2,13 @@ package utils
 
 import "io"
 
+/**
+ * We use io.ReadCloser extensively in the config Connector system, but often we have just an io.Reader to work with.
+ * This struct just allows decorating the Reader with a `func Close() error` and turning it into an io.ReadCloser
+ *
+ * This process is pretty simple, so I didn't write any tests for it.
+ */
+
 // ReaderCloserDecorator A simple struct that wraps an io.Reader and adds a Close() to make it a io.ReadCloser
 type ReaderCloserDecorator struct {
 	io.Reader
